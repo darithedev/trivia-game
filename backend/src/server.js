ddf6e0767e7312169f51d287e8c330d7204a7367
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-02-02 21:59:49 -0500

    Initalize backend server in server.js with Node.js, Express, cors, dotenv, and nodemon dependancies

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', async(req,res) => {
+    res.json({ message: "Express server is healthy."});
+});
+
+app.listen(PORT, '127.0.0.1', () => {
+    console.log(`Express server is running on port ${PORT}`)
+});
