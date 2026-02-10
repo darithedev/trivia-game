b5b74c2b3e13cf1df7058203f51082a13d76e61e
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-02-10 18:38:59 -0500

    create lastFetchedDate and initalize as null

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 let categories = [];
+let lastFetchedDate = null;
 
 app.get('/', async(req,res) => {
     res.json({ message: "Express server is healthy."});
