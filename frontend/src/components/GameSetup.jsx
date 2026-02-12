803c5e22cf9df2f12e55465fd1c8f89a925a7f68
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-02-11 22:44:45 -0500

    Import useState, add state and setter for fetched categories, add amount and difficulty arr, and type obj

--- frontend/src/components/GameSetup.jsx
+++ frontend/src/components/GameSetup.jsx
@@ -1,4 +1,11 @@
+import { useState } from 'react'
+
 const GameSetup = ({ preferences, setPreferences, onPlay }) => {
+    const [fetchedCategories, setFetchedCategories] = useState([]);
+    const amount = [5, 10, 20, 30, 40, 50]; // Number of questions
+    const difficulty = ["easy", "medium", "hard"]
+    const type = { multiple: "Multiple Choice", boolean: "True or False" }
+
     return (
         <div className="game-setup">
             <p>Hello from GameSetup</p>
