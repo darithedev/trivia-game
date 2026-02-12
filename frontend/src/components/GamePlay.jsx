9f3c67a033a7b5fefad14969748f1284749aff14
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-02-12 02:30:36 -0500

    Add parser variable, handleSelection function, and logic to display trivia questions

--- frontend/src/components/GamePlay.jsx
+++ frontend/src/components/GamePlay.jsx
@@ -2,10 +2,38 @@ import { useState } from 'react'
 
 const GamePlay = ({ questionsObj, onSubmit }) => {
     const [userChoices, setUserChoices] = useState({});
+    const parser = new DOMParser();
     
+    const handleSelection = () => {
+
+    }
+
     return (
         <div className="game-play">
             <p>Hello from GamePlay</p>
+            <div>
+                {questionsObj.results.map((question, index) => (
+                    <div key={index}>
+                        <p>{index + 1}: {parser
+                            .parseFromString(`<!doctype html><body>${question.question}`, 'text/html')
+                            .body.textContent}
+                        </p>
+                        {[question.correct_answer, ...question.incorrect_answers].sort().map((choice, index) => (
+                            <button
+                                key={index}
+                                onClick={() => handleSelection(index, parser
+                                    .parseFromString(`<!doctype html><body>${choice}`, 'text/html')
+                                    .body.textContent
+                                )}
+                            >
+                                {parser
+                                    .parseFromString(`<!doctype html><body>${choice}`, 'text/html')
+                                    .body.textContent}
+                            </button>
+                        ))}
+                    </div>
+                ))}
+            </div>
             <button onClick={onSubmit}>Submit</button>
         </div>
     )
