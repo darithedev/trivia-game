48761c3b39a5db7294d9420a4ae004b64877b593
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-02-11 20:08:59 -0500

    Add useState management for user preferences (amount, category, difficulty, and type)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,13 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const [preferences, setPreferences] = useState({ 
+    amount: null, 
+    category: "", 
+    difficulty: "", 
+    type: "",
+  });
+  
   return (
     <>
       
