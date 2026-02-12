bbe73e78f53215c9c812c991ec12eef089bd5345
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-02-11 21:50:48 -0500

    Add passed onPlay prop and Play button to GameSetup component

--- frontend/src/components/GameSetup.jsx
+++ frontend/src/components/GameSetup.jsx
@@ -1,7 +1,8 @@
-const GameSetup = () => {
+const GameSetup = ({ onPlay }) => {
     return (
         <div className="game-setup">
             <p>Hello from GameSetup</p>
+            <button onClick={onPlay}>Play</button>
         </div>
     )
 }
