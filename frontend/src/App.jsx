019a691d923b6eb5171358af556895bfe48bbf9f
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-02-11 20:38:22 -0500

    Add state management for screen and set to setup screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [screen, setScreen] = useState("setup"); // 3 screens: setup, play, result
   const [preferences, setPreferences] = useState({ 
     amount: null, 
     category: "", 
