46d3527f589d3e9ddb3ea21fb7f0b528e45ce239
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-02-12 19:38:24 -0500

    Add GamePlay conditional that renders error message if results array in questionsObj equals to 0, else render questions

--- frontend/src/components/GamePlay.jsx
+++ frontend/src/components/GamePlay.jsx
@@ -17,34 +17,47 @@ const GamePlay = ({ preferences, questionsObj, onSubmit, onExit }) => {
 
     return (
         <div className="game-play">
-            <h2>Trivia Quiz: {preferences.categoryName === "" ? "Mixed Category" : preferences.categoryName}</h2>
-            <div>
-                {questionsObj.results.map((question, index) => (
-                    <div key={index}>
-                        <p>{index + 1}: {decode(question.question)}
-                        </p>
-                        {[question.correct_answer, ...question.incorrect_answers].sort().map((choice, i) => (
-                            <button
-                                key={i}
-                                onClick={() => handleSelection(index, decode(choice))}
-                                style={userChoices[index] === decode(choice)
-                                    ? { backgroundColor: "gray"} 
-                                    : {}
-                                }
-                            >
-                                {decode(choice)}
-                            </button>
-                        ))}
-                    </div>
-                ))}
-            </div>
-            <button
-                disabled={Object.entries(userChoices).length !== questionsObj.results.length}
-                onClick={() => onSubmit(userChoices)}
-            >
-                Submit
-            </button>
-            <button onClick={onExit}>Exit</button>
+            {questionsObj.results.length === 0
+                ? (
+                    <>  
+                        <h2>Oops, there aren't enough questions in this category</h2>
+                        <p>Please reduce the number of questions or change the difficulty and then try again!</p>
+                        <p>If you're still having issues, set the difficulty to "Any Difficulty"</p>
+                        <button onClick={onExit}>Go Back</button>
+                    </>
+                ) : (
+                    <>
+                        <h2>Trivia Quiz: {preferences.categoryName === "" ? "Mixed Category" : preferences.categoryName}</h2>
+                        <div>
+                            {questionsObj.results.map((question, index) => (
+                                <div key={index}>
+                                    <p>{index + 1}: {decode(question.question)}
+                                    </p>
+                                    {[question.correct_answer, ...question.incorrect_answers].sort().map((choice, i) => (
+                                        <button
+                                            key={i}
+                                            onClick={() => handleSelection(index, decode(choice))}
+                                            style={userChoices[index] === decode(choice)
+                                                ? { backgroundColor: "gray"} 
+                                                : {}
+                                            }
+                                        >
+                                            {decode(choice)}
+                                        </button>
+                                    ))}
+                                </div>
+                            ))}
+                        </div>
+                        <button
+                            disabled={Object.entries(userChoices).length !== questionsObj.results.length}
+                            onClick={() => onSubmit(userChoices)}
+                        >
+                            Submit
+                        </button>
+                        <button onClick={onExit}>Exit</button>
+                    </>
+                )
+            }
         </div>
     )
 }
